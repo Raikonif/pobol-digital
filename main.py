0ee7a073adef91f3de6b2b37c18ed0c04daa7256
Raikonif <raikonif@gmail.com>
2025-06-14 11:07:44 -0400

    initial tailwind implementation

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Mount the 'static' directory to serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Initialize Jinja2 templates
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    """
+    This route renders the main HTML page.
+    """
+    return templates.TemplateResponse("index.html", {"request": request})
